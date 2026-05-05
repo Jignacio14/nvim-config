fd0812caadf3a6ad03d9cc0eea456627179cd053
José Ignacio Castro <j14castro2001@gmail.com>
2026-05-04 23:53:50 -0300

    Add mapping for Telescope conflicts command

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>gc", "<cmd>Telescope conflicts<cr>", { desc = "Git conflicts" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
