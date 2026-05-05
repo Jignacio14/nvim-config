a7393376aa80f2c4f08b8fe1ff06ab04e827d4ac
José Ignacio Castro <j14castro2001@gmail.com>
2026-05-04 23:53:18 -0300

    Refactor LSP configuration to add new servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,69 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
-
-local servers = { "html", "cssls" }
-
--- TypeScript/JavaScript/React LSP
-lspconfig.ts_ls.setup({
-  settings = {
-    typescript = {
-      inlayHints = {
-        includeInlayParameterNameHints = "all",
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-    },
-    javascript = {
-      inlayHints = {
-        includeInlayParameterNameHints = "all",
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-    },
-  },
-})
-
--- Go LSP
-lspconfig.gopls.setup({
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-        shadow = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-      usePlaceholders = true,
-      completeUnimported = true,
-      buildFlags = { "-tags=integration" },
-    },
-  },
-})
-
--- Python LSP
-lspconfig.pyright.setup({
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "basic",
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = "workspace",
-      },
-    },
-  },
-})
-
--- Enable other servers
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({})
-end
-
--- read :h vim.lsp.config for changing options of lsp servers
+local servers = {
+  "html",
+  "cssls",
+  "pyright",
+  "gopls",
+  "ruby_lsp",
+  "ts_ls",
+}
+vim.lsp.enable(servers)
